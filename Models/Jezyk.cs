64fcde313e1d278e47b624ae50c787c94e587a9e
HerFx <janiak.m2222@gmail.com>
2022-03-20 13:41:46 +0100

    Dodaj pliki projektów.

---
+++ Models/Jezyk.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AplikacjaDoNaukiJęzyków.Models
+{
+   class Jezyk
+    {
+        private string nazwaJezyka;
+        private string poziomJezyka;
+
+        public string NazwaJezyka
+        {
+            get
+            {
+                return nazwaJezyka;
+            }
+
+            set
+            {
+                nazwaJezyka = value;
+            }
+        }
+
+        public string PoziomJezyka
+        {
+            get
+            {
+                return poziomJezyka;
+            }
+
+            set
+            {
+                poziomJezyka = value;
+            }
+        }
+
+        public Jezyk(string nazwa, string poziom)
+        {
+            this.nazwaJezyka = nazwa;
+            this.poziomJezyka = poziom;
+        }
+    }
+}
---
+++ Models/Slowo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AplikacjaDoNaukiJęzyków.Models
+{
+    public class Slowo
+    {
+        /*private string nazwaSlowo;
+        private string poziomSlowa;
+        private string slowoPol;
+        private string slowoAng;
+        private string slowoAra;
+        */
+
+        private string NazwaSlowo { get; set; }
+        public string SlowoPol { get; set; }
+        public string SlowoAng { get; set; }
+        public string SlowoAra { get; set; }
+
+        public Slowo(string slowoPol, string slowoAng, string slowoAra, string poziom)
+        {
+            this.NazwaSlowo = slowo;
+            this.PoziomSlowa = poziom;
+            this.SlowoPol = slowoPol;
+            this.SlowoAng = slowoAng;
+            this.SlowoAra = slowoAra;
+        }
+    }
+}
